e07e1ecbc2d4d5ba11629a8ee1c030b7e176aa59
Karan-S-Mittal <karanshyammittal@gmail.com>
2020-09-21 00:00:28 +0530

    Updates

---
+++ precache-manifest.89b7c41cb29354d49ac8f28abcbd82f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84f0dd3807b89ae31fa3fb469d965c6",
+    "url": "/react-todoapp/index.html"
+  },
+  {
+    "revision": "fa4f9a6b1f0aa6369354",
+    "url": "/react-todoapp/static/css/main.7b3b0210.chunk.css"
+  },
+  {
+    "revision": "62a99a6af69e6c8aaa13",
+    "url": "/react-todoapp/static/js/2.64394246.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todoapp/static/js/2.64394246.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa4f9a6b1f0aa6369354",
+    "url": "/react-todoapp/static/js/main.f872a517.chunk.js"
+  },
+  {
+    "revision": "fd2cb0e544c7acd91678",
+    "url": "/react-todoapp/static/js/runtime-main.813bcf47.js"
+  }
+]);
